2ed1f806315cc5baec403a3068ecdff054468811
Serge Terryn <47536593+jobraconse@users.noreply.github.com>
2025-09-20 21:01:00 +0200

    Update setupDebianRouterPiZeroW.sh
    disabled ipv6

--- setupDebianRouterPiZeroW.sh
+++ setupDebianRouterPiZeroW.sh
@@ -105,10 +105,13 @@ systemctl enable --now dnsmasq
 echo "[+] ipv4 forwarding in /etc/sysctl.conf..."
 cat >> /etc/sysctl.conf << EOF
 net.ipv4.ip_forward=1
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
 EOF
 
 echo "[+] activating ipv4 forwarding"
-sysctl -w net.ipv4.ip_forward=1
+sysctl -p
 
 echo "[+] add a masquerade for outbound traffic on eth0"
 iptables -t nat -A POSTROUTING -o ETH0 -j MASQUERADE
